2ea22ef791c39c408830d104f948a2a4e31fb1ed
Dharmin Nagar <dharminnagar@gmail.com>
2025-12-12 09:21:01 +0530

    added response handling and 404s

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{BufReader, prelude::*}, 
     net::{TcpListener, TcpStream}
 };
@@ -12,15 +13,31 @@ fn main() {
         handle_request(server_stream);
     }
 
-    fn handle_request(stream: TcpStream) {
+    fn handle_request(mut stream: TcpStream) {
         let reader = BufReader::new(&stream);
-        let http_request: Vec<_> = reader
-            .lines()
-            .map(|result| result.unwrap())
-            .take_while(|line| !line.is_empty())
-            .collect();
-        
-        println!("Request: {http_request:#?}");
+
+        let request_route = reader.lines().next().unwrap().unwrap();
+        // let _http_request: Vec<_> = reader
+        //     .lines()
+        //     .map(|result| result.unwrap())
+        //     .take_while(|line| !line.is_empty())
+        //     .collect();
+
+        if request_route == "GET / HTTP/1.1" {
+            let status_line = "HTTP/1.1 200 OK\r\n\r\n";
+            let contents = fs::read_to_string("public/response.html").unwrap();
+            
+            let response = format!("{status_line} {contents}");
+            
+            stream.write_all(response.as_bytes()).unwrap();
+        } else {
+            let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+            let contents = fs::read_to_string("public/404.html").unwrap();
+
+            let response = format!("{status_line} {contents}");
+
+            stream.write_all(response.as_bytes()).unwrap();
+        }
     }
 
 }
